3fc44e0121afe2ebdea80ac24f3262e6750b925d
MariaBolanosD <maria.bolanos@opendeusto.es>
2023-12-04 21:24:11 +0100

    Funsicona

--- IW_BI_16/IW_BI_16/settings.py
+++ IW_BI_16/IW_BI_16/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'WikiLoL', 'templates', 'chillaid', 'layout', 'style'),
+    os.path.join(BASE_DIR, 'WikiLoL', 'templates'),
 ]
 
 # Default primary key field type
