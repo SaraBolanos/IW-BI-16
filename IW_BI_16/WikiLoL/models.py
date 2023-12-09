2ed0075fb86acc9cf0cd79c5f6681283a891f72e
MariaBolanosD <maria.bolanos@opendeusto.es>
2023-12-09 19:16:03 +0100

    jejejej, a mi me funciona todo :)

--- IW_BI_16/WikiLoL/models.py
+++ IW_BI_16/WikiLoL/models.py
@@ -60,6 +60,7 @@ class Habilidad(models.Model):
     tecla = models.CharField(max_length=15, choices=Tecla.choices, default=Tecla.TECLA_Q)
     campeon = models.ForeignKey(Campeon, on_delete=models.CASCADE, related_name="habilidades")
     imagen = models.TextField(max_length=100)
+    video = models.TextField(max_length=500, null = True, blank= True)
     def __str__(self):
         return self.nombre 
 
